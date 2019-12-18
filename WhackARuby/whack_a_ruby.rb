e474c988313da1ccb225e558b71190d0f6da3142
Saif Abu <saif.abu@cerner.com>
2019-12-17 22:33:30 -0600

    adding code

--- WhackARuby/whack_a_ruby.rb
+++ WhackARuby/whack_a_ruby.rb
@@ -4,6 +4,15 @@ class WhackARuby < Gosu::Window
   def initialize
     super(800, 600)
     self.caption = "whack the ruby!"
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+  end
+
+  def draw
+    @image.draw(@x - @width / 2, @y - @height/2, 1)
   end
 end
 
