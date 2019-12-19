1a4e86a53897b2a7993050639f1b55ef7b4d42c2
Saif Abu <saif.abu@cerner.com>
2019-12-18 23:37:43 -0600

    adding code

--- WhackARuby/whack_a_ruby.rb
+++ WhackARuby/whack_a_ruby.rb
@@ -9,12 +9,9 @@ class WhackARuby < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
-    @velocity_x = 5
-    @velocity_y = 5
-  end
-
-  def draw
-    @image.draw(@x - @width / 2, @y - @height/2, 1)
+    @velocity_x = 1
+    @velocity_y = 1
+    @visible = 0
   end
 
   def update
@@ -23,6 +20,15 @@ class WhackARuby < Gosu::Window
 
     @velocity_x *= -1 if @x + @width/2 >= 800 || @x - @width/2 <= 0
     @velocity_y *= -1 if @y + @height/2 >= 600 || @y - @height/2 <= 0
+
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
+  end
+
+  def draw
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height/2, 1)
+    end
   end
 end
 
