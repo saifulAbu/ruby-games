a77b94bb886c923c2e075bca42c12cc2f11a1cdb
Saif Abu <saif.abu@cerner.com>
2020-01-04 20:34:30 -0600

    adding squares

--- twelve/game.rb
+++ twelve/game.rb
@@ -6,12 +6,17 @@ class Game
     @squares = []
     color_list = []
     [:red, :green, :blue].each do |color|
-      (0..5).each do |row|
-        (0..5).each do |column|
-          index = row * 6 + column
-          @squares.push Square.new(@window, column, row, color_list[index])
-        end
+      12.times do
+        color_list.push color
       end
+    end
+    color_list.shuffle!
+    (0..5).each do |row|
+      (0..5).each do |column|
+        index = row * 6 + column
+        @squares.push Square.new(@window, column, row, color_list[index])
+      end
+    end
     end
   end
   @font = Gosu::Font.new(36)
