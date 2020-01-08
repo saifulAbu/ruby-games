a3ddce4ad3a66c9d591d88fe094494179f81155c
Saif Abu <saif.abu@cerner.com>
2020-01-07 22:50:48 -0600

    adding more stuffs

--- twelve/twelve.rb
+++ twelve/twelve.rb
@@ -1,1 +1,17 @@
-#dummy
+require 'gosu'
+require_relative 'game'
+
+class Twelve < Gosu::Window
+  def initialize
+    super(640, 640)
+    self.caption = "twelve"
+    @game = Game.new(self)
+  end
+
+  def draw
+    @game.draw
+  end
+end
+
+window = Twelve.new
+window.show
