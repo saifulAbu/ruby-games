ba85e89f5ed18180a4bebdabf35fabc010bbc4b8
Saif Abu <saif.abu@cerner.com>
2020-02-14 06:37:06 -0600

    adding more stuffs

--- escape/boulder.rb
+++ escape/boulder.rb
@@ -7,6 +7,32 @@ class Boulder
     @body = CP::BODY.new(400, 4000)
     @body.p = CP::Vec2.new(x, y)
     @body.v_limit = SPEED_LIMIT
+
+    bounds = [CP::Vec2.new(-13, -6),
+              CP::Vec2.new(-16, -4),
+              CP::Vec2.new(-16, 6),
+              CP::Vec2.new(-3, 12),
+              CP::Vec2.new(8, 12),
+              CP::Vec2.new(13, 10),
+              CP::Vec2.new(16, 3),
+              CP::Vec2.new(16, -4),
+              CP::Vec2.new(10, -9),
+              CP::Vec2.new(2, -11)
+            ]
+    shape = CP::Shape::Poly.new(@body, bounds, CP::Vec2.new(0, 0))
+    shape.u = FRICTION
+    shape.e = ELASTICITY
+    @width = 32
+    @height = 32
+    window.space.add_body(@body)
+    window.space.add_shape(shape)
+    @image = Gosu::Image.new('images/boulder.png')
+    @body.apply_impluse(CP::Vec2.new(rand(100000) - 50000, 100000),
+                        CP::Vec2.new(rand * 0.8 - 0.4, 0))
+  end
+
+  def draw
+    @image.draw_rot(@body.p.z, @body.p.y, 1, @body.angle * (180.0 / Math::PI))
   end
 end
 
--- escape/escape.rb
+++ escape/escape.rb
@@ -1,9 +1,11 @@
 require 'gosu'
 require 'chipmunk'
+require 'boulder'
 
 class Escape < Gosu::Window
   DAMPING = 0.90
   GRAVITY = 400.0
+  BOULDER_FREQUENCY = 0.01
 
 	attr_reader :space
 
@@ -15,15 +17,23 @@ class Escape < Gosu::Window
     @background = Gosu::Image.new('images/background.png', tileable: true)
     @space.damping = DAMPING
     @space.gravity = CP::Vec2.new(0.0, GRAVITY)
+    @boulders = []
   end
 
   def draw
-    @background.draw(0, 0, 1)
+    @background.draw(0, 0, 0)
+    @background.draw(0, 529, 0)
+    @boulders.each do |boulder|
+      boulder.draw
+    end
   end
 
   def udpate
     unless @game_over
       @space.step(1.0/600)
+    end
+    if rand < BOULDER_FREQUENCY
+      @boulders.push Boulder.new(self, 200 + rand(400), -20)
     end
   end
 end
