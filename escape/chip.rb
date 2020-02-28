48da97c352353bda135d4b78e413238e028a29a3
Saiful Abu <saifulabu25@gmail.com>
2020-02-27 23:55:09 -0600

    adding chip code

--- escape/chip.rb
+++ escape/chip.rb
@@ -1,4 +1,13 @@
 class Chip
-    RUN_IMPLUSE = 600
-    FLY_IMPLUSE = 60
+    RUN_IMPULSE = 600
+    FLY_IMPULSE = 60
+    JUMP_IMPULSE = 36000
+    AIR_JUMP_IMPULSE = 1200
+    SPEED_LIMIT = 400
+    FRICTION = 0.7
+    ELASTICITY = 0.2
+    attr_accessor :off_ground
+
+    def initialize(window, x, y)
+    end
 end
