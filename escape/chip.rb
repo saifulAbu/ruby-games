3e3c00b933b5d2ee6ad1f19c4c9d70c0092821e5
Saiful Abu <saifulabu25@gmail.com>
2020-03-01 22:50:02 -0600

    adding chip code

--- escape/chip.rb
+++ escape/chip.rb
@@ -9,5 +9,23 @@ class Chip
     attr_accessor :off_ground
 
     def initialize(window, x, y)
+        @window = window
+        space = window.space
+        @images = Gosu::Image.load_tiles('images/chip.png', 40, 65)
+        @body = CP::Body.new(50, 100 / 0.0)
+        @body.p = CP::Vec2.new(x, y)
+        @body.v_limit = SPEED_LIMIT
+        bounds = [CP::Vec2.new(-10, -32),
+                    CP::Vec2.new(-10, 32),
+                    CP::Vec2.new(10, 32),
+                    CP::Vec2.new(10, -32)]
+        shape = CP::Shape::Poly.new(@body, bounds, CP::Vec2.new(0, 0))
+        shape.u = FRICTION
+        shape.e = ELASTICITY
+        space.add_body(@body)
+        space.add_shape(shape)
+        @action = :stand
+        @image_index = 0
+        @off_ground = true
     end
 end
