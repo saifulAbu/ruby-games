603ff7a33d97354fbc338bc6f227080020315f98
Saiful Abu <saifulabu25@gmail.com>
2020-03-10 20:37:21 -0500

    Adding chip

--- escape/escape.rb
+++ escape/escape.rb
@@ -3,6 +3,7 @@ require 'chipmunk'
 require_relative 'boulder'
 require_relative 'platform'
 require_relative 'wall'
+require_relative 'chip'
 
 class Escape < Gosu::Window
   DAMPING = 0.90
@@ -25,6 +26,7 @@ class Escape < Gosu::Window
     @floor = Wall.new(self, 400, 810, 800, 20)
     @left_wall = Wall.new(self, -10, 400, 20, 800)
     @right_wall = Wall.new(self, 810, 470, 20, 660)
+    @player = Chip.new(self, 70, 700)
   end
 
   def draw
